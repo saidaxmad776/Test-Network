34de425815b03ab53eee5016496d0b5b83b14093
ASaidaxmad <97520969+saidaxmad776@users.noreply.github.com>
2022-10-15 20:13:38 +0500

    GET and POST request

--- TestNetwork/ViewController.swift
+++ TestNetwork/ViewController.swift
@@ -7,7 +7,7 @@
 
 import UIKit
 
-class ViewController: UIViewController {
+class ViewControllerSet: UIViewController {
     
     
     override func viewDidLoad() {
@@ -15,5 +15,52 @@ class ViewController: UIViewController {
         
     }
     
+    @IBAction func getRequest(_ sender: UIButton) {
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let session = URLSession.shared
+        session.dataTask(with: url) { data, response, error in
+            guard let response = response,
+                  let data = data else { return }
+            print(response)
+            print(data)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+    }
     
+    @IBAction func postRequest(_ sender: UIButton) {
+        
+        guard let url = URL(string: "https://jsonplaceholder.typicode.com/posts") else { return }
+        
+        let userData = ["Course": "Networking", "Lesson": "GET and POST",]
+        
+        var request = URLRequest(url: url)
+        request.httpMethod = "POST"
+        
+        guard let httpBody = try? JSONSerialization.data(withJSONObject: userData, options: []) else { return }
+        request.httpBody = httpBody
+        request.addValue("application/json", forHTTPHeaderField: "Content-Type")
+        
+        let session = URLSession.shared
+        session.dataTask(with: request) { (data, response, error) in
+            
+            guard let response = response, let data = data else { return }
+            print(response)
+            
+            do {
+                let json = try JSONSerialization.jsonObject(with: data, options: [])
+                print(json)
+            } catch {
+                print(error)
+            }
+            
+        }.resume()
+    }
 }
