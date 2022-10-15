905f14127d5f1af1acf9d7f212e0ab02436ce31a
ASaidaxmad <97520969+saidaxmad776@users.noreply.github.com>
2022-10-15 16:23:56 +0500

    Initial Commit

---
+++ TestNetwork/ViewController.swift
@@ -0,0 +1,19 @@
+//
+//  ViewController.swift
+//  TestNetwork
+//
+//  Created by Saidaxmad on 15/10/22.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view.
+    }
+
+
+}
+
